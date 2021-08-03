63054537645fbdd343d57ae8885011371edab7f9
Ronaldo Paulino <ronaldopln_jau@hotmail.com>
2021-08-02 21:49:25 -0300

    Adicionei as cores grass e poison e a navegação entre telas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'charmander',
+    loadChildren: () => import('./pokemons/charmander/charmander.module').then( m => m.CharmanderPageModule)
+  },
 ];
 
 @NgModule({
