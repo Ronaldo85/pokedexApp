2f94bd8a175121cef9be10369c0f60111de1e036
Ronaldo Paulino <ronaldopln_jau@hotmail.com>
2021-08-02 22:23:59 -0300

    Criamos o Array de pokemons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  //array de pokemons
+  // [] => representa um array (lista)
+  // {} => representa um objeto (item)
+  listaPokemon = [
+    {
+      numero: '004',
+      nome: 'Charmander',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+      tipos: [
+        'Fire'
+      ]
+    },
+    {
+      numero: '025',
+      nome: 'Pikachu',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+      tipos: [
+        'Eletric'
+      ]
+    },
+    {
+      numero: '119',
+      nome: 'Seaking',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/119.png',
+      tipos: [
+        'Water'
+      ]
+    },
+    {
+      numero: '123',
+      nome: 'Scyther',
+      foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/123.png',
+      tipos: [
+        'Bug', 'Flying'
+      ]
+    },
+
+  ];
+
   constructor() {}
 
 }
